d6dd3a195680f58fc847c1f0df53b571249ead21
Trenton Knight <tknightj1995@yahoo.com>
2019-10-10 16:32:07 +0000

    setup

---
+++ traffic-light/src/Traffic.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function TrafficLight() {
+	return (
+		<div>
+			<div className="light">
+				<div id="red" className="red1"></div>
+				<div id="yellow" className="yellow1"></div>
+				<div id="green" className="green1"></div>
+			</div>
+            <div className="pole"></div>
+
+			<div id="walkLight" className="walkLite">
+				<div id="walk" className="walk"></div>
+				<div id="doNotWalk" className="doNotWalk"></div>
+			</div>
+
+			<div id="controls" className="configBoxx">
+				<div id="walk" className="walk" />
+				<div id="doNotWalk" className="doNotWalk" />
+			</div>
+		</div>
+	);
+}
+
+export default TrafficLight;
