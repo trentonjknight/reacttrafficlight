71ff305dc55650492610869890876df76953939c
audioryan9 <ryandsgn@outlook.com>
2019-10-10 19:56:19 +0000

    asdas

--- traffic-light/src/Traffic.js
+++ traffic-light/src/Traffic.js
@@ -1,55 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 
 function TrafficLight() {
-    const [red, setRed] = useState('red1 lightOff')
-    const [yellow,setYellow] = useState('yellow1 lightOff')
-    const [green, setGreen] = useState('green1 lightOff')
-
 	return (
 		<div>
 			<div className="light">
-				<div id="red" className={red}
-                    onClick={() => {
-                    setRed('red1 lightOn')
-                    setYellow('yellow1 lightOff')
-                    setGreen('green1 lightOff')
-                }}>
-                </div>
-
-				<div id="yellow" className={yellow}
-                    onClick={() => {
-                    setYellow('yellow1 lightOn')
-                    setRed('red1 lightOff')
-                    setGreen('green1 lightOff')
-                }}>
-                </div>
-
-				<div id="green" className={green}
-                    onClick={() => {
-                    setGreen('green1 lightOn')
-                    setRed('red1 lightOff')
-                    setYellow('yellow1 lightOff')
-                }}>
-                </div>
-
+				<div id="red" className="red1"></div>
+                <div id="yellow" className="yellow1"></div>
+				<div id="green" className="green1"></div>
 			</div>
             <div className="pole"></div>
 
-			<div className="walkLite">
+			<div id="walkLight" className="walkLite">
 				<div id="walk" className="walk">WALK</div>
                 <br></br>
 				<div id="doNotWalk" className="doNotWalk">DONT WALK</div>
-                <div className="pole2"></div>
-                <div className="settings">
-                    <div id="on">ON</div>
-                    <br></br>
-                    <div id="off" >OFF</div>
-                    <br></br>
-                    <div id="auto">AUTO</div>
-                </div>
+            <div className="pole2"></div>
 			</div>
+
+			<div id="controls" className="settings">
+				<div id="on">ON</div>
+                <br></br>
+				<div id="off" >OFF</div>
+                <br></br>
+                <div id="auto">AUTO</div>
+			</div>
+
 		</div>
 	);
 }
 
 export default TrafficLight;
+
+
---
+++ traffic-light/src/crosswalk.js
@@ -0,0 +1,43 @@
+
+<div>
+			<div className="lightShow">
+				<div id="redOne" className="redOne">
+                    <img src = "https://github.com/trentonjknight/reacttrafficlight/blob/33a2fe431e76620e2ade5ef798ec9175bbf1fa7e/images/red-light.png"></img>
+            </div>
+            </div>
+                <div id="yellowOne" className="yellowOne">
+                    <img src = "https://github.com/trentonjknight/reacttrafficlight/blob/33a2fe431e76620e2ade5ef798ec9175bbf1fa7e/images/yellow-light.png"></img>
+            </div>
+				<div id="greenOne" className="greenOne">
+                    <img src = "https://github.com/trentonjknight/reacttrafficlight/blob/33a2fe431e76620e2ade5ef798ec9175bbf1fa7e/images/green-light.png"></img>
+            </div>
+            <div>
+                    <video width="1920px" height="1080px" controls>
+                    <source src="https://github.com/trentonjknight/reacttrafficlight/blob/33a2fe431e76620e2ade5ef798ec9175bbf1fa7e/images/City%20-%206383.mp4"
+                    type="video/mp4"></source>
+                    </video>
+            </div>
+
+
+</div>
+
+
+
+            {/* <div className="pole"></div> */}
+
+			{/* <div id="walkLight" className="walkLite">
+				<div id="walk" className="walk">WALK</div>
+                <br></br>
+				<div id="doNotWalk" className="doNotWalk">DONT WALK</div>
+            <div className="pole2"></div>
+			</div>
+
+			<div id="controls" className="settings">
+				<div id="on">ON</div>
+                <br></br>
+				<div id="off" >OFF</div>
+                <br></br>
+                <div id="auto">AUTO</div>
+			</div> */}
+
+		{/* </div> */}
