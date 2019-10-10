2f7bef3b3f3d9b6a96fb901f05e41c3214dfaacd
Trenton Knight <tknightj1995@yahoo.com>
2019-10-10 19:55:19 +0000

    here

--- traffic-light/src/Traffic.js
+++ traffic-light/src/Traffic.js
@@ -33,21 +33,28 @@ function TrafficLight() {
                 </div>
 
 			</div>
+
             <div className="pole"></div>
 
-			<div className="walkLite">
-				<div id="walk" className="walk">WALK</div>
-                <br></br>
-				<div id="doNotWalk" className="doNotWalk">DONT WALK</div>
+            <div>
+
+                <div className="walkLight">
+                    <div className="walk">
+
+                    </div>
+                    <div className="doNotWalk">
+                    <img class="man" src="https://image.flaticon.com/icons/png/512/8/8818.png"></img>
+                    </div>
+                </div>
                 <div className="pole2"></div>
+
                 <div className="settings">
                     <div id="on">ON</div>
-                    <br></br>
                     <div id="off" >OFF</div>
-                    <br></br>
                     <div id="auto">AUTO</div>
                 </div>
-			</div>
+            </div>
+
 		</div>
 	);
 }
