2a1c7b74b0b6c9bfae55be8267df7fee1eef8f11
Trenton Knight <tknightj1995@yahoo.com>
2019-10-11 13:36:05 +0000

    help

--- traffic-light/src/Traffic.js
+++ traffic-light/src/Traffic.js
@@ -1,7 +1,7 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 
 function TrafficLight() {
-<<<<<<< HEAD
     const [red, setRed] = useState('red1 lightOff')
     const [yellow,setYellow] = useState('yellow1 lightOff')
     const [green, setGreen] = useState('green1 lightOff')
@@ -11,47 +11,41 @@ function TrafficLight() {
         setTimeout(()=> setMils(mils+1),1000)
     },[mils])
 
-=======
->>>>>>> 86ea8725d3fc6793b226798d1acf23a4790a14ee
+
 	return (
 		<div>
 			<div className="light">
-				<div id="red" className="red1"></div>
-                <div id="yellow" className="yellow1"></div>
-				<div id="green" className="green1"></div>
+				<div id="red" className={red}
+                    onClick={() => {
+                    setRed('red1 lightOn')
+                    setYellow('yellow1 lightOff')
+                    setGreen('green1 lightOff')
+                }}>
+                </div>
+
+				<div id="yellow" className={yellow}
+                    onClick={() => {
+                    setYellow('yellow1 lightOn')
+                    setRed('red1 lightOff')
+                    setGreen('green1 lightOff')
+                }}>
+                </div>
+
+				<div id="green" className={green}
+                    onClick={() => {
+                    setGreen('green1 lightOn')
+                    setRed('red1 lightOff')
+                    setYellow('yellow1 lightOff')
+                }}>
+                </div>
 			</div>
 
-            <div className="pole"></div>
+                <div className="pole"></div>
 
-			<div id="walkLight" className="walkLite">
-				<div id="walk" className="walk">WALK</div>
-                <br></br>
-				<div id="doNotWalk" className="doNotWalk">DONT WALK</div>
-            <div className="pole2"></div>
-			</div>
-
-			<div id="controls" className="settings">
-				<div id="on">ON</div>
-                <br></br>
-				<div id="off" >OFF</div>
-                <br></br>
-                <div id="auto">AUTO</div>
-			</div>
             <div>
-
-<<<<<<< HEAD
                 <div className="walkLight">
-
                     <div className={red.includes('On') ? 'man' : 'walkMan man'}>
-                    {/* <div className={red === 'red1 lightOff' ? '' : ''}> */}
-
                         <img class="man" src="https://image.flaticon.com/icons/png/512/8/8818.png"></img>
-=======
-                {/* <div className="walkLight">
-                    <div className="walk">
->>>>>>> 86ea8725d3fc6793b226798d1acf23a4790a14ee
-
-                    </div>
 
                     <div className={green.includes('On') ? 'hand man' : 'doNotWalk'}>
 
@@ -67,15 +61,13 @@ function TrafficLight() {
                     <br></br>
                     <button id="off" >OFF</button>
                     <br></br>
-
                     <div className={mils} onClick={() => {setMils('auto')}}>Auto
                     </div>
 
                 </div>
-            </div> */}
-
 		</div>
-		</div>
+	</div>
+</div>
 	);
 }
 
